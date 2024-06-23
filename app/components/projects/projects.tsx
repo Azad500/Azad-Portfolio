1eb2477df855c76ad47f26905406ef47ae14de8a
Azad500 <azad.mirzazada95@gmail.com>
2024-06-23 17:55:58 +0400

    sixth commit

--- app/components/projects/projects.tsx
+++ app/components/projects/projects.tsx
@@ -3,6 +3,21 @@ import Slider from "./slider";
 import styles from "./projects.module.scss";
 import GarantCompressor from "../../../assets/projects/GarantCompressor.png";
 import Webrandy from "../../../assets/projects/Webrandy.png";
+import AddressArch from "../../../assets/projects/AddressArch.png";
+import AlasEva from "../../../assets/projects/AlasEva.png";
+import AzSpeech from "../../../assets/projects/AzSpeech.png";
+import MStock from "../../../assets/projects/MStock.png";
+import BestPrint from "../../../assets/projects/BestPrint.png";
+import LosAngeles from "../../../assets/projects/LosAngeles.png";
+import AmoFresh from "../../../assets/projects/AmoFresh.png";
+import StudentAll from "../../../assets/projects/StudentAll.png";
+import MayakCargo from "../../../assets/projects/MayakCargo.png";
+import Minik from "../../../assets/projects/Minik.png";
+import MayakMebel from "../../../assets/projects/MayakMebel.png";
+import PutaLogistik from "../../../assets/projects/PutaLogistik.png";
+import Mico from "../../../assets/projects/Mico.png";
+import MenzilTap from "../../../assets/projects/MenzilTap.png";
+import AzadPortfolio from "../../../assets/projects/AzadPortfolio.png";
 
 const freelanceElement = [
   {
@@ -10,6 +25,36 @@ const freelanceElement = [
     alt: "Garant Compressor",
     link: "https://garantcompressor.com/",
     site: "garantcompressor.com",
+  },
+  {
+    image: LosAngeles,
+    alt: "Los Angeles",
+    link: "https://los-angeles-zeta.vercel.app/",
+    site: "losangeles.com",
+  },
+  {
+    image: AmoFresh,
+    alt: "AmoFresh",
+    link: "https://amofresh.az/",
+    site: "amoFresh.az",
+  },
+  {
+    image: StudentAll,
+    alt: "StudentAll",
+    link: "https://studentall.com/studentinfo/",
+    site: "studentall.com/studentinfo",
+  },
+  {
+    image: Mico,
+    alt: "Mico",
+    link: "https://mico-theta.vercel.app/",
+    site: "mico-cinema.com",
+  },
+  {
+    image: AzadPortfolio,
+    alt: "AzadPortfolio",
+    link: "...",
+    site: "AzadMirzazada.me",
   },
 ];
 
@@ -19,6 +64,72 @@ const webrandyStartuup = [
     alt: "Webrandy Startup",
     link: "https://webrandy.com/",
     site: "webrandy.com",
+  },
+  {
+    image: AddressArch,
+    alt: "Address Arch",
+    link: "https://address-arch.vercel.app/",
+    site: "address-arch.com",
+  },
+  {
+    image: BestPrint,
+    alt: "Best Print",
+    link: "...",
+    site: "bestprint.az",
+  },
+];
+
+const alasElement = [
+  {
+    image: AlasEva,
+    alt: "Alas Eva",
+    link: "...",
+    site: "alasdevcenter.com",
+  },
+  {
+    image: AzSpeech,
+    alt: "AzSpeech",
+    link: "...",
+    site: "azspeech.az",
+  },
+];
+
+const mstock = [
+  {
+    image: MStock,
+    alt: "MStock",
+    link: "https://mstock.az/",
+    site: "mstock.az",
+  },
+  {
+    image: MayakCargo,
+    alt: "MayakCargo",
+    link: "https://cargo-pearl-chi.vercel.app/",
+    site: "mayakcargo.az",
+  },
+  {
+    image: Minik,
+    alt: "Minik",
+    link: "...",
+    site: "minik.az",
+  },
+  {
+    image: MayakMebel,
+    alt: "MayakMebel",
+    link: "https://mayak-mebel.vercel.app/",
+    site: "mayakMebel.az",
+  },
+  {
+    image: PutaLogistik,
+    alt: "PutaLogistik",
+    link: "https://puta-logistik.vercel.app/",
+    site: "puta-logistik.az",
+  },
+  {
+    image: MenzilTap,
+    alt: "MenzilTap",
+    link: "...",
+    site: "menzilTap.az",
   },
 ];
 
@@ -33,46 +144,100 @@ export default function Projects() {
             <li>
               <h3>Freelance</h3>
             </li>
-            {freelanceElement.map((element, index) => (
-              <li key={index}>
-                <div className={styles.imageElement}>
-                  <Image
-                    className={styles.image}
-                    src={element.image}
-                    alt={element.alt}
-                    width={400}
-                    height={400}
-                    priority
-                  />
+            <li className={styles.elements}>
+              {freelanceElement.map((element, index) => (
+                <div className={styles.imageContainer} key={index}>
+                  <div className={styles.imageElement}>
+                    <Image
+                      className={styles.image}
+                      src={element.image}
+                      alt={element.alt}
+                      width={400}
+                      height={400}
+                      priority
+                    />
+                  </div>
+                  <a target="_blank" href={element.link}>
+                    {element.site}
+                  </a>
                 </div>
-                <a target="_blank" href={element.link}>
-                  {element.site}
-                </a>
-              </li>
-            ))}
+              ))}
+            </li>
           </ul>
           {/* ---------------------- */}
           <ul className={styles.portfolioPart}>
             <li>
               <h3>Webrandy Startup</h3>
             </li>
-            {webrandyStartuup.map((element, index) => (
-              <li key={index}>
-                <div className={styles.imageElement}>
-                  <Image
-                    className={styles.image}
-                    src={element.image}
-                    alt={element.alt}
-                    width={400}
-                    height={400}
-                    priority
-                  />
+            <li className={styles.elements}>
+              {webrandyStartuup.map((element, index) => (
+                <div className={styles.imageContainer} key={index}>
+                  <div className={styles.imageElement}>
+                    <Image
+                      className={styles.image}
+                      src={element.image}
+                      alt={element.alt}
+                      width={400}
+                      height={400}
+                      priority
+                    />
+                  </div>
+                  <a target="_blank" href={element.link}>
+                    {element.site}
+                  </a>
                 </div>
-                <a target="_blank" href={element.link}>
-                  {element.site}
-                </a>
-              </li>
-            ))}
+              ))}
+            </li>
+          </ul>
+          {/* ---------------------- */}
+          <ul className={styles.portfolioPart}>
+            <li>
+              <h3>Alas Development Center</h3>
+            </li>
+            <li className={styles.elements}>
+              {alasElement.map((element, index) => (
+                <div className={styles.imageContainer} key={index}>
+                  <div className={styles.imageElement}>
+                    <Image
+                      className={styles.image}
+                      src={element.image}
+                      alt={element.alt}
+                      width={400}
+                      height={400}
+                      priority
+                    />
+                  </div>
+                  <a target="_blank" href={element.link}>
+                    {element.site}
+                  </a>
+                </div>
+              ))}
+            </li>
+          </ul>
+          {/* ---------------------- */}
+          <ul className={styles.portfolioPart}>
+            <li>
+              <h3>MSTOCK Company</h3>
+            </li>
+            <li className={styles.elements}>
+              {mstock.map((element, index) => (
+                <div className={styles.imageContainer} key={index}>
+                  <div className={styles.imageElement}>
+                    <Image
+                      className={styles.image}
+                      src={element.image}
+                      alt={element.alt}
+                      width={400}
+                      height={400}
+                      priority
+                    />
+                  </div>
+                  <a target="_blank" href={element.link}>
+                    {element.site}
+                  </a>
+                </div>
+              ))}
+            </li>
           </ul>
         </div>
       </div>
--- app/components/projects/slider.tsx
+++ app/components/projects/slider.tsx
@@ -1,114 +1,200 @@
 import styles from "./slider.module.scss";
 import Image from "next/image";
-import AdasDiploma from "../../../assets/certifications/AdasDiploma.png";
-import DusunceAcademy from "../../../assets/certifications/DusunceAcademy.png";
-import AlasDiploma from "../../../assets/certifications/AlasDiploma.png";
-import HonoursAcademy from "../../../assets/certifications/HonoursAcademy.png";
-import ElcPro from "../../../assets/certifications/ElcPro.png";
-import AzFin from "../../../assets/certifications/AzFin.png";
-import MasterDegree from "../../../assets/certifications/MasterDegree.png";
-import BachelorDegree from "../../../assets/certifications/BachelorDegree.png";
-import School257 from "../../../assets/certifications/School257.png";
+import GarantCompressor from "../../../assets/projects/GarantCompressor.png";
+import Webrandy from "../../../assets/projects/Webrandy.png";
+import AddressArch from "../../../assets/projects/AddressArch.png";
+import AlasEva from "../../../assets/projects/AlasEva.png";
+import AzSpeech from "../../../assets/projects/AzSpeech.png";
+import MStock from "../../../assets/projects/MStock.png";
+import BestPrint from "../../../assets/projects/BestPrint.png";
+import LosAngeles from "../../../assets/projects/LosAngeles.png";
+import AmoFresh from "../../../assets/projects/AmoFresh.png";
+import StudentAll from "../../../assets/projects/StudentAll.png";
+import MayakCargo from "../../../assets/projects/MayakCargo.png";
+import Minik from "../../../assets/projects/Minik.png";
+import MayakMebel from "../../../assets/projects/MayakMebel.png";
+import PutaLogistik from "../../../assets/projects/PutaLogistik.png";
+import Mico from "../../../assets/projects/Mico.png";
+import MenzilTap from "../../../assets/projects/MenzilTap.png";
+import AzadPortfolio from "../../../assets/projects/AzadPortfolio.png";
 
 const cardElements = [
   {
-    image: AdasDiploma,
-    alt: "AdasDiploma",
+    id: 1,
+    image: GarantCompressor,
+    alt: "GarantCompressor",
+    link: "https://garantcompressor.com/",
   },
   {
-    image: DusunceAcademy,
-    alt: "DushunceDiploma",
+    id: 2,
+    image: Webrandy,
+    alt: "Webrandy",
+    link: "https://webrandy.com/",
   },
   {
-    image: AlasDiploma,
-    alt: "AlasDiploma",
+    id: 3,
+    image: AddressArch,
+    alt: "AddressArch",
+    link: "https://address-arch.vercel.app/",
   },
   {
-    image: HonoursAcademy,
-    alt: "HonoursDiploma",
+    id: 4,
+    image: AlasEva,
+    alt: "AlasEva",
+    link: "...",
   },
   {
-    image: ElcPro,
-    alt: "ElcProDiploma",
+    id: 5,
+    image: AzSpeech,
+    alt: "AzSpeech",
+    link: "...",
   },
   {
-    image: AzFin,
-    alt: "AzFinDiploma",
+    id: 6,
+    image: MStock,
+    alt: "MStock",
+    link: "https://mstock.az/",
   },
   {
-    image: MasterDegree,
-    alt: "MasterDiploma",
+    id: 7,
+    image: BestPrint,
+    alt: "BestPrint",
+    link: "...",
   },
   {
-    image: BachelorDegree,
-    alt: "BachelorDiploma",
+    id: 8,
+    image: LosAngeles,
+    alt: "LosAngeles",
+    link: "https://los-angeles-zeta.vercel.app/",
   },
   {
-    image: School257,
-    alt: "School257Diploma",
+    id: 9,
+    image: AmoFresh,
+    alt: "AmoFresh",
+    link: "https://amofresh.az/",
+  },
+  {
+    id: 10,
+    image: StudentAll,
+    alt: "StudentAll",
+    link: "https://studentall.com/studentinfo/",
+  },
+  {
+    id: 11,
+    image: MayakCargo,
+    alt: "MayakCargo",
+    link: "https://cargo-pearl-chi.vercel.app/",
+  },
+  {
+    id: 12,
+    image: Minik,
+    alt: "Minik",
+    link: "...",
+  },
+  {
+    id: 13,
+    image: MayakMebel,
+    alt: "MayakMebel",
+    link: "https://mayak-mebel.vercel.app/",
+  },
+  {
+    id: 14,
+    image: PutaLogistik,
+    alt: "PutaLogistik",
+    link: "https://puta-logistik.vercel.app/",
+  },
+  {
+    id: 15,
+    image: Mico,
+    alt: "Mico",
+    link: "https://mico-theta.vercel.app/",
+  },
+  {
+    id: 16,
+    image: MenzilTap,
+    alt: "MenzilTap",
+    link: "...",
+  },
+  {
+    id: 17,
+    image: AzadPortfolio,
+    alt: "AzadPortfolio",
+    link: "...",
   },
 ];
-const repeatedClientsData = Array.from(
+const repeatedData = Array.from(
   { length: 30 },
   (_, index) => cardElements[index % cardElements.length]
 );
+
+// --------reverse------
+const reverseData = Array.from(
+  { length: 30 },
+  (_, index) => cardElements[index % cardElements.length]
+).reverse();
 export default function Slider() {
   return (
     <>
       {/* --------------first-slider----------- */}
       <div className={styles.sliderContainer}>
         <div className={styles.firstSliderContainer}>
-          {Array.from({ length: 20 }, (_, index) => (
+          {Array.from({ length: 30 }, (_, index) => (
             <div className={styles.firstSliderElement} key={index}>
-              {repeatedClientsData.map((element, elementIndex) => (
-                <div className={styles.slider} key={elementIndex}>
-                  <Image
-                    className={styles.image}
-                    src={element.image}
-                    alt={element.alt}
-                    width={400}
-                    height={400}
-                    priority
-                  />
-                </div>
+              {repeatedData.map((element, elementIndex) => (
+                <a target="_blank" href={element.link}>
+                  <div className={styles.slider} key={elementIndex}>
+                    <Image
+                      className={styles.image}
+                      src={element.image}
+                      alt={element.alt}
+                      width={400}
+                      height={400}
+                      priority
+                    />
+                  </div>
+                </a>
               ))}
             </div>
           ))}
         </div>
         {/* --------------second-slider----------- */}
         <div className={styles.secondSliderContainer}>
-          {Array.from({ length: 20 }, (_, index) => (
+          {Array.from({ length: 30 }, (_, index) => (
             <div className={styles.secondSliderElement} key={index}>
-              {repeatedClientsData.map((element, elementIndex) => (
-                <div className={styles.slider} key={elementIndex}>
-                  <Image
-                    className={styles.image}
-                    src={element.image}
-                    alt={element.alt}
-                    width={400}
-                    height={400}
-                    priority
-                  />
-                </div>
+              {repeatedData.map((element, elementIndex) => (
+                <a target="_blank" href={element.link}>
+                  <div className={styles.slider} key={elementIndex}>
+                    <Image
+                      className={styles.image}
+                      src={element.image}
+                      alt={element.alt}
+                      width={400}
+                      height={400}
+                      priority
+                    />
+                  </div>
+                </a>
               ))}
             </div>
           ))}
         </div>
         {/* --------------third-slider----------- */}
         <div className={styles.firstSliderContainer}>
-          {Array.from({ length: 20 }, (_, index) => (
+          {Array.from({ length: 30 }, (_, index) => (
             <div className={styles.firstSliderElement} key={index}>
-              {repeatedClientsData.map((element, elementIndex) => (
-                <div className={styles.slider} key={elementIndex}>
-                  <Image
-                    className={styles.image}
-                    src={element.image}
-                    alt={element.alt}
-                    width={400}
-                    height={400}
-                    priority
-                  />
-                </div>
+              {reverseData.map((element, elementIndex) => (
+                <a target="_blank" href={element.link}>
+                  <div className={styles.slider} key={elementIndex}>
+                    <Image
+                      className={styles.image}
+                      src={element.image}
+                      alt={element.alt}
+                      width={400}
+                      height={400}
+                      priority
+                    />
+                  </div>
+                </a>
               ))}
             </div>
           ))}
