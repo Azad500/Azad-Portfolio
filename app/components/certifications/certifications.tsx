bff3e4ca5803d8c68b95f5d997950644b495d479
Azad500 <azad.mirzazada95@gmail.com>
2024-06-26 19:46:29 +0400

    91%

--- app/components/certifications/certifications.tsx
+++ app/components/certifications/certifications.tsx
@@ -1,4 +1,4 @@
-import PureCardFlip from "../cardFlip/PureCardFlip";
+import PureCardFlip from "../../../app/components/cardFlip/PureCardFlip";
 import styles from "./certifications.module.scss";
 export default function Certifications() {
   return (
